960209c926fc6d6adeb58f2477b907c898864a80
johnny lamb <agnusjack@gmail.com>
2015-07-13 15:32:38 -0600

    forgot to push up code

---
+++ 99bottlesofbeer.js
@@ -0,0 +1,27 @@
+for (var i = 99; i > 0; i --) {
+
+    if(i===2) {
+
+    console.log(i + " bottles of beer on the wall");
+    console.log(i + " bottles of beer!");
+    console.log("take one down pass it around");
+    console.log(i-1+ " bottle of beer on the wall");
+    console.log(" ");
+    console.log( "1 bottle of beer on the wall");
+    console.log( "1 bottle of beer!");
+    console.log("take one down pass it around");
+    console.log("0 freaking! bottles of beer on the wall!");
+    console.log("this is the end!");
+    console.log(" ");
+    break;
+    }
+
+  else(i>=3 && i!= 1);
+
+    console.log(i + " bottles of beer on the wall");
+    console.log(i + " bottles of beer!");
+    console.log("take one down pass it around");
+    console.log(i-1+ " bottles of beer on the wall");
+    console.log(" ");
+
+  }
